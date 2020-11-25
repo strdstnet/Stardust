20ad1533075738db3a526fc3612f1e6c8edc64bf
Dylan Ogle <delxhq0@gmail.com>
2020-11-25 09:30:38 +0000

    Fix ContainerClose packet.

--- src/network/bedrock/ContainerClose.ts
+++ src/network/bedrock/ContainerClose.ts
@@ -4,6 +4,7 @@ import { DataType } from '../../types/data'
 
 interface IContainerClose {
   windowId: number,
+  server?: boolean,
 }
 
 export class ContainerClose extends BatchedPacket<IContainerClose> {
@@ -11,6 +12,7 @@ export class ContainerClose extends BatchedPacket<IContainerClose> {
   constructor(p?: IContainerClose) {
     super(Packets.CONTAINER_CLOSE, [
       { name: 'windowId', parser: DataType.BYTE },
+      { name: 'server', parser: DataType.BOOLEAN, resolve: () => false },
     ])
 
     if(p) this.props = p
